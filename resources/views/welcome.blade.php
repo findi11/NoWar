80d6a74fb3e96572b8f1d637567cca343f970a17
Vlad Grebeniuk <Grebeniuk1180406@gmail.com>
2022-11-23 17:35:16 +0200

    Додано головний слайдер на сайт v 0.1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,35 @@
     Головна
 @endsection
 
-@section('content')
 
+@section('content')
+    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="./public/images/21210.jpg" alt="First slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="./public/images/Bohdana_self-propelled_howitzer_(cropped).jpg"
+                    alt="Second slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="./public/images/01001p68-0d01_1280x720.jpeg"
+                    alt="Second slide">
+            </div>
+           
+            <div class="carousel-item">
+                <img class="d-block w-100"
+                    src="./public/images/384.jpg"
+                    alt="Third slide">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only"></span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only"></span>
+        </a>
+    </div>
 @endsection
